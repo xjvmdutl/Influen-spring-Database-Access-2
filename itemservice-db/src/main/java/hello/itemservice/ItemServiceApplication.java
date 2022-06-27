66efa3f30d5be2698b5c7e2df03a3495ab9cab59
Kimjunho <widn45@naver.com>
2022-06-27 11:30:57 +0900

    MyBatis Pratice

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -15,7 +15,8 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
-@Import(JdbcTemplateV3Config.class)
+//@Import(JdbcTemplateV3Config.class)
+@Import(MyBatisConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
